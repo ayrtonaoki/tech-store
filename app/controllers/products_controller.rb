c6808ee91d2d7ca271a79d149112b2738ffcfcf8
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-05-24 03:08:54 -0300

    Create create method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,10 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def create
+    product = params.require(:product).permit(:name, :description, :price, :amount)
+    Product.create(product)
+    redirect_to root_path
+  end
 end
