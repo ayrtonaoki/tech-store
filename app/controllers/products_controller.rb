7071259a9b5ca1cc86dd8176f92990ba36a545a5
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-05-27 20:09:39 -0300

    Create search method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,4 +13,9 @@ class ProductsController < ApplicationController
     Product.destroy(params[:id])
     redirect_to root_path
   end
+
+  def search
+    @name = params[:name]
+    @products = Product.where("name ilike ?", "%#{@name}%")
+  end
 end
