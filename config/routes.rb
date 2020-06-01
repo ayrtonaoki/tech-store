b6f614726829f5fcd29cba5fe9672f8335b3f4cf
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-01 20:49:08 -0300

    Create scaffold for products category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   root to: 'products#index'
   resources :products, only: [:new, :create, :destroy]
   get "products/search", to: "products#search", as: :search_products
