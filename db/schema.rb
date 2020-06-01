78fcb46072ae518ac86159832fcd5a814a3d3ef5
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-01 20:50:33 -0300

    Create schema for categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_053414) do
+ActiveRecord::Schema.define(version: 2020_06_01_234827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
