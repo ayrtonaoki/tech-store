b8070597457a21f1726e07c15fe304974312c3e1
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-01 21:07:50 -0300

    Update code of category create and destroy method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,44 +1,29 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
-
-  # GET /categories
-  # GET /categories.json
   def index
     @categories = Category.all
   end
 
-  # GET /categories/1
-  # GET /categories/1.json
   def show
   end
 
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
   def edit
   end
 
-  # POST /categories
-  # POST /categories.json
   def create
-    @category = Category.new(category_params)
-
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    values = params.require(:category).permit(:name)
+    @category = Category.new(values)
+    if @category.save
+      flash[:notice] = "#{@category.name} was created"
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /categories/1
-  # PATCH/PUT /categories/1.json
   def update
     respond_to do |format|
       if @category.update(category_params)
@@ -51,14 +36,9 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.json
   def destroy
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    Category.destroy(params[:id])
+    redirect_to root_path
   end
 
   private
