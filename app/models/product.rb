57742bd95bc1b7738a2b3dca95cdac99c5dce041
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-01 21:40:05 -0300

    Update product model, set product belong to category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  belongs_to :category
+
   validates :name, length: { minimum: 3 }
   validates :description, length: { minimum: 5 }
   validates :price, :amount, presence: true
