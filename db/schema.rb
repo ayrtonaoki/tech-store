20bb54e8c0efe7d369c16c16629fbcdf6835cef5
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-01 21:40:50 -0300

    Create schema for category id of product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_234827) do
+ActiveRecord::Schema.define(version: 2020_06_02_003247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_06_01_234827) do
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
