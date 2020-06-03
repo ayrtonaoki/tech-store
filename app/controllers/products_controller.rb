2280e87cb2a0cc1f502a053d685e794189c802ab
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-02 21:43:34 -0300

    Create edit and update method for product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,27 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    id = params[:id]
+    @product = Product.find(id)
+    @categories = Category.all
+    render :new
+  end
+
+  def update
+    id = params[:id]
+    @product = Product.find(id)
+
+    values = params.require(:product).permit(:name, :description, :price, :amount, :category_id)
+    if @product.update(values)
+      flash[:notice] = "#{@product.name} was updated"
+      redirect_to root_path
+    else
+      @categories = Category.all
+      render :new
+    end
+  end
+
   def destroy
     Product.destroy(params[:id])
     redirect_to root_path
