1d893623a557b36df0e14089459125a6749a3a44
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-02 21:44:51 -0300

    Update products resources: add edit and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :categories
   root to: 'products#index'
-  resources :products, only: [:new, :create, :destroy]
+  resources :products, only: [:new, :create, :destroy, :edit, :update]
   get "products/search", to: "products#search", as: :search_products
 end
