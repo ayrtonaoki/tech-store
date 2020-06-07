4a363b6c2236a6264f09fa57874afeaaa0042438
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-06 22:52:10 -0300

    Update product model: add category presence validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ApplicationRecord
 
   validates :name, length: { minimum: 3 }
   validates :description, length: { minimum: 5 }
-  validates :price, :amount, presence: true
+  validates :price, :amount, :category, presence: true
 end
