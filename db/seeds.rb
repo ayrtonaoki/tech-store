e7d34ba183bf264c5a13d4cf344e17f640b42db7
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-21 19:49:05 -0300

    Create seeds for Product and Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create!(
+  name: 'Smartphones'
+)
+
+Product.create!(
+  name: 'Apple iPhone 11 Pro Max',
+  description: '6.5-inch Super Retina XDR OLED display, 12MP TrueDepth front camera',
+  price: 1099.00,
+  amount: 10,
+  category_id: Category.last.id
+)
+
+Category.create!(
+  name: 'Video Games'
+)
+
+Product.create!(
+  name: 'Nintendo Switch Neon Red Blue',
+  description: 'This model includes battery life of approximately 4.5 to 9 hours',
+  price: 441.26,
+  amount: 20,
+  category_id: Category.last.id
+)
+
+Category.create!(
+  name: 'Hardware'
+)
+
+Product.create!(
+  name: 'Intel Core i9-9900K',
+  description: '8 Cores up to 5.0GHz Unlocked LGA1151 300 Series 95W',
+  price: 509.00,
+  amount: 5,
+  category_id: Category.last.id
+)
